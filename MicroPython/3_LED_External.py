f6a874de83380e178e6c25c3e68f072002de42bc
Deepak <j.thepac@gmail.com>
2023-11-25 19:10:41 +0530

    External LED

--- MicroPython/3_LED_External.py
+++ MicroPython/3_LED_External.py
@@ -1,1 +1,11 @@
-# LED external
+# Pin1 is GP0
+
+from machine import Pin
+import time
+led = Pin(0, Pin.OUT)  # Pin 1
+for i in range(1, 5):
+    print(i)
+    led.value(1)
+    time.sleep(1)  # Use 1 second instead of 10 seconds for better visibility
+    led.value(0)
+    time.sleep(1)# LED external
